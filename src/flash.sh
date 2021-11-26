82dc2619a57666bf99f1baa049cbe66fb47fc53d
Jonathan Giles <jonathangiles@fastmail.fm>
2021-11-26 12:42:39 -0500

    More configuration ...

--- src/flash.sh
+++ src/flash.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-/Users/jgiles/tools/openocd/bin/openocd -f interface/cmsis-dap.cfg -f target/lpc8xx-jg.cfg -c 'program ../build/blinky.bin 0x00000000' -c 'sleep 200' -c 'reset run' -c 'exit'
+/Users/jgiles/tools/openocd/bin/openocd -f interface/cmsis-dap.cfg -f target/lpc8xx-jg.cfg -c 'program ./build/blinky.bin 0x00000000' -c 'sleep 200' -c 'reset run' -c 'exit'
+
