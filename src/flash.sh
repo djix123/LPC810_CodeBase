7f9374e7b3ab9cc561d4d4b2abcd21eca4be8065
Jonathan Giles <jonathangiles@fastmail.fm>
2021-11-26 10:46:16 -0500

    1. Add flash script 2. Create map file 3. Clean map file and build dir 4. Add flash target

---
+++ src/flash.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+/Users/jgiles/tools/openocd/bin/openocd -f interface/cmsis-dap.cfg -f target/lpc8xx-jg.cfg -c 'program ../build/blinky.bin 0x00000000' -c 'sleep 200' -c 'reset run' -c 'exit'
